9857b76e584b92dc267e564a26f35977bb864b05
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-05 22:03:12 +0530

    feat: use react query for greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { GreetingResponseSchema } from "./types/GreetingResponse";
 
 const App = () => {
-  const [greeting, setGreeting] = useState("");
   const [loading, isLoading] = useState(true);
   const [error, setError] = useState({ isError: false, message: "" });
 
-  const fetchGreeting = async () => {
-    try {
+  const GetGreeting = useQuery({
+    queryKey: ["greeting"],
+    queryFn: async () => {
       const response = await fetch("http://localhost:5271");
-      const data = GreetingResponseSchema.parse(await response.json());
-      setGreeting(data.message);
-    } catch (error) {
+      const data = await response.json();
+
+      return GreetingResponseSchema.parse(data);
+    },
+    enabled: true,
+    onError: () => {
       setError({
         isError: true,
         message: "Something went wrong. Please try again!",
       });
-    } finally {
+    },
+    onSettled: () => {
       isLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchGreeting();
-
-    return () => {
-      setGreeting("");
-      isLoading(true);
-      setError({ isError: false, message: "" });
-    };
-  }, []);
+    },
+  });
 
   return (
     <>
-      {!loading && !error.isError && <p>{greeting}</p>}
+      <p>{GetGreeting.data?.message}</p>
 
       {loading && <p>Loading...</p>}
 
