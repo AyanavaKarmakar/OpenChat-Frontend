716b72dd42b21387553807c95c66a69e1968455e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-05 20:46:44 +0530

    feat: render greetings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { GreetingResponseSchema } from "./types/GreetingResponse";
+
 const App = () => {
-  return <>Hello, World!</>;
+  const [greeting, setGreeting] = useState("");
+  const [loading, isLoading] = useState(true);
+  const [error, setError] = useState({ isError: false, message: "" });
+
+  const fetchGreeting = async () => {
+    try {
+      const response = await fetch("http://localhost:5271");
+      const data = GreetingResponseSchema.parse(await response.json());
+      setGreeting(data.message);
+    } catch (error) {
+      setError({
+        isError: true,
+        message: "Something went wrong. Please try again!",
+      });
+    } finally {
+      isLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchGreeting();
+
+    return () => {
+      setGreeting("");
+      isLoading(true);
+      setError({ isError: false, message: "" });
+    };
+  }, []);
+
+  return (
+    <>
+      {!loading && !error.isError && <p>{greeting}</p>}
+
+      {loading && <p>Loading...</p>}
+
+      {error.isError && <p>{error.message}</p>}
+    </>
+  );
 };
 
 export default App;
