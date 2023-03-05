721b08acbaace6527b2133d3c8874b0d5ccc906f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-06 01:08:17 +0530

    feat: add theme toggle icons

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,6 +1,11 @@
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import Brightness4SharpIcon from "@mui/icons-material/Brightness4Sharp";
+import BrightnessHighSharpIcon from "@mui/icons-material/BrightnessHighSharp";
 
 export const Navbar = () => {
+  const [themeToggle, setThemeToggle] = useState("light");
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -8,6 +13,22 @@ export const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             OPENCHAT
           </Typography>
+
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="theme-toggle"
+            onClick={() =>
+              setThemeToggle(themeToggle === "dark" ? "light" : "dark")
+            }
+          >
+            {themeToggle === "dark" ? (
+              <BrightnessHighSharpIcon />
+            ) : (
+              <Brightness4SharpIcon />
+            )}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
