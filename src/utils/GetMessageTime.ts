0570f648b0e52d4972c0a3dd3f13d43c3d3f2c05
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-07 23:20:17 +0530

    feat: get message time

---
+++ src/utils/GetMessageTime.ts
@@ -0,0 +1,26 @@
+export const GetMessageTime = (messageDate: Date): string => {
+  const today = new Date();
+
+  const diff = today.getTime() - messageDate.getTime();
+  const diffMinutes = diff / 1000 / 60;
+  const diffHours = diff / 1000 / 60 / 60;
+
+  let dateString: string;
+  if (messageDate.toDateString() === today.toDateString()) {
+    if (diffMinutes < 1) {
+      dateString = "sent just now";
+    } else if (diffMinutes < 60) {
+      dateString = `sent ${Math.round(diffMinutes)} ${
+        Math.round(diffMinutes) === 1 ? "minute ago" : "minutes ago"
+      }`;
+    } else {
+      dateString = `sent ${Math.round(diffHours)} ${
+        Math.round(diffHours) === 1 ? "hour ago" : "hours ago"
+      }`;
+    }
+  } else {
+    dateString = `sent on ${messageDate.toLocaleDateString()}`;
+  }
+
+  return dateString;
+};
